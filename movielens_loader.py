596881d6f1583f09e6d870db6e011cece65c7573
habroptilus-smn-mac <hikaru_ogura@so-netmedia.jp>
2019-11-28 18:56:44 +0900

    治った

--- movielens_loader.py
+++ movielens_loader.py
@@ -1,53 +1,49 @@
 import numpy as np
 import pandas as pd
-import codecs
 from scipy.sparse import csr_matrix
 from pathlib import Path
+from sklearn.feature_extraction import DictVectorizer
 
 
 class MovielensLoader:
-    def __init__(self, data_dir=Path("./ml-100k"), log_filename="u.data", item_filename="u.item", user_filename="u.user"):
-        self.log_path = data_dir / log_filename
-        self.item_path = data_dir / item_filename
+    def __init__(self, data_dir=Path("./ml-100k"), user_filename="u.user"):
+        self.data_dir = data_dir
         self.user_path = data_dir / user_filename
 
-    def create_dataset(self):
-        items = self.load_items()
+    def create_dataset(self, include_context_features=True):
+        """datasetを作成.
+        :param include_context_features: context featureをXに含めるか
+        """
         users = self.load_users()
-        logs, y = self.load_log_and_ratings()
-        features_df = self.merge(logs, users, items)
-        X = csr_matrix(features_df.values, dtype=np.float)
-        return X, y
+        df_train, y_train = self.load_log_and_ratings(log_filename="ua.base")
+        df_test, y_test = self.load_log_and_ratings(log_filename="ua.test")
+        if include_context_features:
+            df_train = self.merge(df_train, users)
+            df_test = self.merge(df_test, users)
+        train_data = self.to_dict(df_train)
+        test_data = self.to_dict(df_test)
 
-    def load_log_and_ratings(self, drop_columns=["timestamp"]):
-        logs = pd.read_csv(self.log_path, names=["uid", "mid", "rating", "timestamp"], sep="\t")
-        ratings = np.array(logs["rating"])
+        self.vectorizer = DictVectorizer()
+        X_train = self.vectorizer.fit_transform(train_data)
+        X_test = self.vectorizer.transform(test_data)
+        X_train = csr_matrix(X_train, dtype=np.float)
+        X_test = csr_matrix(X_test, dtype=np.float)
+        return X_train, y_train, X_test, y_test
+
+    def to_dict(self, df):
+        return df.to_dict(orient='records')
+
+    def load_log_and_ratings(self, log_filename, drop_columns=["timestamp"]):
+        logs = pd.read_csv(self.data_dir / log_filename, names=["uid", "mid", "rating", "timestamp"], sep="\t", dtype=str)
+        ratings = np.array(logs["rating"], dtype=np.float)
         drop_columns.append("rating")
         logs = logs.drop(drop_columns, axis=1)
         return logs, ratings
 
     def load_users(self, drop_columns=["age", "zip_code"]):
-        users = pd.read_csv(self.user_path, names=["uid", "age", "gender", "occupation", "zip_code"], sep="|")
+        users = pd.read_csv(self.user_path, names=["uid", "age", "gender", "occupation", "zip_code"], sep="|", dtype=str)
         users = users.drop(drop_columns, axis=1)
         return users
 
-    def load_items(self, drop_columns=["title", "released", "video_released", "IMDb_URL"]):
-        """itemのcontext追加情報の読み込み
-        read_csvだとUnicodeDecodeErrorが起きてしまったのでhttps://qiita.com/niwaringo/items/d2a30e04e08da8eaa643で解決
-        後ろのcolumnはジャンルを表す.(複数ジャンルに属するitemもあるらしい)
-        """
-
-        with codecs.open(self.item_path, "r", "Shift-JIS", "ignore") as f:
-            items = pd.read_table(f, names=["mid", "title", "released", "video_released", "IMDb_URL",
-                                            "unknown", "Action", "Adventure", "Animation",
-                                            "Children", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
-                                            "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci_Fi",
-                                            "Thriller", "War", "Western"], delimiter="|")
-        items = items.drop(drop_columns, axis=1)
-        return items
-
-    def merge(self, logs, users, items):
-        log_user = pd.merge(logs, users, on="uid")
-        log_user_dummied = pd.get_dummies(log_user)
-        merged_df = pd.merge(log_user_dummied, items, on="mid")
-        return pd.get_dummies(merged_df, columns=["uid", "mid"])
+    def merge(self, logs, users):
+        return pd.merge(logs, users, on="uid")
